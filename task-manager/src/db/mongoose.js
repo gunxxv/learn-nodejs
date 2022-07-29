e9ae490e6ea415507a411d668c8bd6fae5b840bd
kannika <kannika@braindynamics.co.th>
2022-07-29 18:44:52 +0700

    90

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,62 +1,18 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 
-const User = mongoose.model('Users', {
-    name: {
+const Task = mongoose.model('Tasks', {
+    description: {
         type: String,
-        required: true,
-        
+        trim: true,
+        require: true
     },
-    email: {
-        type: String,
-        require: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
+    completed: {
+        type: Boolean,
+        default: false
     }
 })
 
-const me = new User({
-    name: 'Mike',
-    email: 'mike@'
-})
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error', error)
-})
-
-
-// const Task = mongoose.model('Tasks', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
-// })
-
-// const me = new Task({
-//     description: 'Pot tree',
-//     completed: true
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((erroe) => {
-//     console.log('Error', console.error())
-// })
