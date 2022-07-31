2cdd224a41ff65cbf50cd38333740d7ebe6ce41f
kan <gunxxv@live.com>
2022-08-01 00:05:16 +0700

    95

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -3,16 +3,3 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 
-const Task = mongoose.model('Tasks', {
-    description: {
-        type: String,
-        trim: true,
-        require: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-
---
+++ task-manager/src/models/task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const Task = mongoose.model('Tasks', {
+    description: {
+        type: String,
+        trim: true,
+        require: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = Task
