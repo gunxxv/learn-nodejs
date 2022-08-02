f4b7f493c94ef4c4c3d11c69ff14a90b952555ca
NerdyDawin <nerdy@email.com>
2022-08-02 19:05:06 +0700

    97-2

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,36 +8,61 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
-        res.status(201).send(user)            
-    }).catch((e) => {
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
+
+    // user.save().then(() => {
+    //     res.status(201).send(user)            
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((user) => {
-        res.send(user)
-    }).catch((e) => {
+app.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({})
+        res.status(200).send(users)
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+    // User.find({}).then((user) => {
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
-    User.findById(_id).then((user) => {
+    try {
+        const user = await findById(_id)
+
         if (!user) {
-            return res.status(404).send()
+            return res.status(404).send(user)
         }
-        res.status(200).send(user)
-    }).catch((e) => {
-        res.status(500).send()
-    })
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send()
+    //     }
+    //     res.status(200).send(user)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 app.post('/tasks', (req, res) => {
